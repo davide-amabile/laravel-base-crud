e767d20d0287a88825a27bed88865b5cf14aa42a
DAVIDE <davideamabile81gmail.com>
2020-10-28 15:02:20 +0100

    rotte

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', "BookController@index")->name("books.index");
+// Route::get('/', "BookController@index")->name("books.index");
+
+Route::resource("books", "BookController");
